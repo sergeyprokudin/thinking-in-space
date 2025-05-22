7f603098f52b7b4db1607c2253eef97a6ad8feb9
Sergey Prokudin <sergey.prokudin@gmail.com>
2025-05-22 17:28:25 +0200

    llava-one-vision inference

--- evaluate_single.sh
+++ evaluate_single.sh
@@ -18,7 +18,10 @@ num_processes=4
 num_frames=32
 launcher=accelerate
 
+#available_models="llava_one_vision_qwen2_0p5b_ov_32f,llava_one_vision_qwen2_7b_ov_32f,llava_next_video_7b_qwen2_32f,llama3_vila1p5_8b_32f,llama3_longvila_8b_128frames_32f,longva_7b_32f,internvl2_2b_8f,internvl2_8b_8f"
+
 available_models="llava_one_vision_qwen2_0p5b_ov_32f"
+
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -154,7 +157,6 @@ for model in "${models[@]}"; do
         --tasks $benchmark \
         --batch_size 1 \
         --log_samples \
-        --verbosity DEBUG \
         --log_samples_suffix $model \
         --output_path $output_path/$benchmark \
         "
